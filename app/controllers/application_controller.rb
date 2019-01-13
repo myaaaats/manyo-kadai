b99fc2c5137f644183499296cc36b6ca2a892f0b
Maya <maya_sato@diveintocode.jp>
2019-01-13 19:36:59 +0900

    Basic認証の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_certifiaction
+
+  private
+
+  def basic_certifiaction
+    authenticate_or_request_with_http_basic do |name, password|
+      name == ENV['BASIC_NAME'] && password == ENV['BASIC_PASS']
+    end
+  end
 end
